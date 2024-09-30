7b64c20fb16ec7d1b69e28d78ec16d637f73d89f
Williamsanchez-01 <williamcostasanchez@gmail.com>
2024-09-30 12:17:55 -0300

    finaliza exercicio ImitandoDory

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+Console.WriteLine("Imitando Dory\n");
+
+Console.Write("Digite uma frase para ser traduzida para baleies : ");
+string f = Console.ReadLine()!;
+
+string traducao = f
+    .Replace("a", "aaa")
+    .Replace("A", "AAA")
+    .Replace("e", "eee")
+    .Replace("E", "EEE")
+    .Replace("i", "iii")
+    .Replace("I", "III")
+    .Replace("o", "ooo")
+    .Replace("O", "OOO")
+    .Replace("u", "uuu")
+    .Replace("U", "UUU")
+;
+
+Console.WriteLine($"Em baleiês: {traducao}");
+
+Console.WriteLine("Pronto agora voce ja sabe como se fala isso em baleies!!");
